72681bf7c90cf0042cc233642afa24a0c5b2887a
paduvireh <paduvireh@unjbg.edu.pe>
2024-09-06 15:11:19 -0500

    Se agrego el archivo suma de a numeros

---
+++ sumaPrimerosNum.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a;
+	int suma;
+	cout<<"Ingrese el numero con el cual desea trabajar: "<<endl;
+	cin>>a;
+	suma=(a*(a+1))/2;
+	cout<<"La suma de los a primero numeros es: "<<suma;
+	return 0;
+}
+
