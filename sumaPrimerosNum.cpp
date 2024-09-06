33d7de61bd281feb746b501a8245a588f1843d90
paduvireh <paduvireh@unjbg.edu.pe>
2024-09-06 15:58:48 -0500

    Se agrego funciones al archivo sumatoria hasta un numero

--- sumaPrimerosNum.cpp
+++ sumaPrimerosNum.cpp
@@ -1,12 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-	int a;
-	int suma;
-	cout<<"Ingrese el numero con el cual desea trabajar: "<<endl;
-	cin>>a;
-	suma=(a*(a+1))/2;
-	cout<<"La suma de los a primero numeros es: "<<suma;
-	return 0;
+int sumar(int n){
+    int suma=0;
+    for (int i=1; i<=n; i++){
+        suma+= i;
+    }
+    return suma;
 }
-
+int main(){
+    int n;
+    cout<<"Ingresa un numero: ";
+    cin>>n;
+    int resultado=sumar(n);
+    cout<<"La suma de los numeros desde 1 hasta " <<n<< " es: " <<resultado<< endl;
+    return 0;
+}
